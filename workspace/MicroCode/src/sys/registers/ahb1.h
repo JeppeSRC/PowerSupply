de2c21fc569ec4df338f34a88ddb777c2408a520
JeppeSRC <jeppe97rc@gmail.com>
2019-04-07 20:08:23 +0200

    RCC registers

--- workspace/MicroCode/src/sys/registers/ahb1.h
+++ workspace/MicroCode/src/sys/registers/ahb1.h
@@ -1,6 +1,8 @@
 #ifndef SYS_REGISTERS_AHB1_H_
 #define SYS_REGISTERS_AHB1_H_
 
+#pragma region DMA
+
 struct DMA_ISR {
 	//Channel 1
 	unsigned char GIF1 : 1;
@@ -129,5 +131,273 @@ struct DMAx {
 
 #define DMA1 ((struct DMAx*)0x40020000)
 #define DMA2 ((struct DMAx*)0x40020400)
+
+#pragma endregion
+
+#pragma region RCC
+
+struct RCC_CR {
+	unsigned char HSION : 1;
+	unsigned char HSIRDY : 1;
+	unsigned char reserved : 1;
+	unsigned char HSITRIM : 5;
+	unsigned char HSICAL : 8;
+	unsigned char HSEON : 1;
+	unsigned char HSERDY : 1;
+	unsigned char HSEBYP : 1;
+	unsigned char CSSON : 1;
+	unsigned char reserved1 : 4;
+	unsigned char PLLON : 1;
+	unsigned char PLLRDY : 1;
+	unsigned char reserved2 : 6;
+};
+
+struct RCC_CFGR {
+	unsigned char SW : 2;
+	unsigned char SWS : 2;
+	unsigned char HPRE : 4;
+	unsigned char PPRE1 : 3;
+	unsigned char PPRE2 : 3;
+	unsigned char ADCPRE : 2;
+	unsigned char PLLSRC : 1;
+	unsigned char PLLXTPRE : 1;
+	unsigned char PLLMUL : 4;
+	unsigned char USBPRE : 1;
+	unsigned char reserved : 1;
+	unsigned char MCO : 3;
+	unsigned char SDPRE : 5;
+};
+
+struct RCC_CIR {
+	unsigned char LSIRDYF : 1;
+	unsigned char LSERDYF : 1;
+	unsigned char HSIRDYF : 1;
+	unsigned char HSERDYF : 1;
+	unsigned char PLLRDYF : 1;
+	unsigned char reserved : 2;
+	unsigned char CSSF : 1;
+	unsigned char LSIRDYIE : 1;
+	unsigned char LSERDYIE : 1;
+	unsigned char HSIRDYIE : 1;
+	unsigned char HSERDYIE : 1;
+	unsigned char PLLRDYIE : 1;
+	unsigned char reserved1 : 3;
+	unsigned char LSIRDYC : 1;
+	unsigned char LSERDYC : 1;
+	unsigned char HSIRDYC : 1;
+	unsigned char HSERDYC : 1;
+	unsigned char PLLRDYC : 1;
+	unsigned char reserved2 : 2;
+	unsigned char CSSC : 1;
+	unsigned char reserved3 : 8;
+};
+
+struct RCC_APB2RSTR {
+	unsigned char SYSCFGRST : 1;
+	unsigned char reserved : 8;
+	unsigned char ADCRST : 1;
+	unsigned char reserved1 : 2;
+	unsigned char SPI1RST : 1;
+	unsigned char reserved2 : 1;
+	unsigned char USART1RST : 1;
+	unsigned char reserved3 : 1;
+	unsigned char TIM15RST : 1;
+	unsigned char TIM16RST : 1;
+	unsigned char TIM17RST : 1;
+	unsigned char TIM19RST : 1;
+	unsigned char reserved4 : 4;
+	unsigned char SDAD1RST : 1;
+	unsigned char SDAD2RST : 1;
+	unsigned char SDAD3RST : 1;
+	unsigned char reserved5 : 5;
+};
+
+struct RCC_APB1RSTR {
+	unsigned char TIM2RST : 1;
+	unsigned char TIM3RST : 1;
+	unsigned char TIM4RST : 1;
+	unsigned char TIM5RST : 1;
+	unsigned char TIM6RST : 1;
+	unsigned char TIM7RST : 1;
+	unsigned char TIM12RST : 1;
+	unsigned char TIM13RST : 1;
+	unsigned char TIM14RST : 1;
+	unsigned char TIM18RST : 1;
+	unsigned char reserved : 1;
+	unsigned char WWDGRST : 1;
+	unsigned char reserved2 : 2;
+	unsigned char SPI2RST : 1;
+	unsigned char SPI3RST : 1;
+	unsigned char reserved3 : 1;
+	unsigned char USART2RST : 1;
+	unsigned char USART3RST : 1;
+	unsigned char reserved3 : 2;
+	unsigned char I2C1RST : 1;
+	unsigned char I2C2RST : 1;
+	unsigned char USBRST : 1;
+	unsigned char reserved4 : 1;
+	unsigned char CANRST : 1;
+	unsigned char DAC2RST : 1;
+	unsigned char reserved5 : 1;
+	unsigned char PWRRST : 1;
+	unsigned char DAC1RST : 1;
+	unsigned char CECRST : 1;
+	unsigned char reserved6 : 1;
+};
+
+struct RCC_AHBENR {
+	unsigned char DMAEN : 1;
+	unsigned char DMA2EN : 1;
+	unsigned char SRAMEN : 1;
+	unsigned char reserved : 1;
+	unsigned char FLITFEN : 1;
+	unsigned char reserved2 : 1;
+	unsigned char CRCEN : 1;
+	unsigned short reserved3 : 10;
+	unsigned char IOPAEN : 1;
+	unsigned char IOPBEN : 1;
+	unsigned char IOPCEN : 1;
+	unsigned char IOPDEN : 1;
+	unsigned char IOPEEN : 1;
+	unsigned char IOPFEN : 1;
+	unsigned char reserved3 : 1;
+	unsigned char TSCEN : 1;
+	unsigned char reserved4 : 7;
+
+};
+
+struct RCC_APB2ENR {
+	unsigned char SYSCFGEN : 1;
+	unsigned char reserved : 8;
+	unsigned char ADCEN : 1;
+	unsigned char reserved2 : 2;
+	unsigned char SPI1EN : 1;
+	unsigned char reserved3 : 1;
+	unsigned char USART1EN : 1;
+	unsigned char reserved4 : 1;
+	unsigned char TIM15EN : 1;
+	unsigned char TIM16EN : 1;
+	unsigned char TIM17EN : 1;
+	unsigned char TIM19EN : 1;
+	unsigned char reserved5 : 2;
+	unsigned char DBGMCUEN : 1;
+	unsigned char reserved6 : 1;
+	unsigned char SDADC1EN : 1;
+	unsigned char SDADC2EN : 1;
+	unsigned char SDADC3EN : 1;
+	unsigned char reserved7 : 5;
+
+};
+
+struct RCC_APB1ENR {
+	unsigned char TIM2EN : 1;
+	unsigned char TIM3EN : 1;
+	unsigned char TIM4EN : 1;
+	unsigned char TIM5EN : 1;
+	unsigned char TIM6EN : 1;
+	unsigned char TIM7EN : 1;
+	unsigned char TIM12EN : 1;
+	unsigned char TIM13EN : 1;
+	unsigned char TIM14EN : 1;
+	unsigned char TIM18EN : 1;
+	unsigned char reserved : 1;
+	unsigned char WWDGEN : 1;
+	unsigned char reserved2 : 2;
+	unsigned char SPI2EN : 1;
+	unsigned char SPI3EN : 1;
+	unsigned char reserved3 : 1;
+	unsigned char USART2EN : 1;
+	unsigned char USART3EN : 1;
+	unsigned char reserved3 : 2;
+	unsigned char I2C1EN : 1;
+	unsigned char I2C2EN : 1;
+	unsigned char USBEN : 1;
+	unsigned char reserved4 : 1;
+	unsigned char CANEN : 1;
+	unsigned char DAC2EN : 1;
+	unsigned char reserved5 : 1;
+	unsigned char PWREN : 1;
+	unsigned char DAC1EN : 1;
+	unsigned char CECEN : 1;
+	unsigned char reserved6 : 1;
+};
+
+struct RCC_BDCR {
+	unsigned char LSEON : 1;
+	unsigned char LSERDT : 1;
+	unsigned char LSEBYP : 1;
+	unsigned char LSEDRV : 2;
+	unsigned char reserved : 3;
+	unsigned char RTCSEL : 2;
+	unsigned char reserved2 : 5;
+	unsigned char RTCEN : 1;
+	unsigned char RDRST : 1;
+	unsigned short reserved3 : 15;
+};
+
+struct RCC_CSR {
+	unsigned char LSION : 1;
+	unsigned char LSIRDY : 1;
+	unsigned int reserved : 21;
+	unsigned char V18PWRRSTF : 1;
+	unsigned char RMVF : 1;
+	unsigned char OBLRSTF : 1;
+	unsigned char PINRSTF : 1;
+	unsigned char PORRSTF : 1;
+	unsigned char SFTRSTF : 1;
+	unsigned char IWDGRSTF : 1;
+	unsigned char WWDGRSTF : 1;
+	unsigned char LPWRRSTF : 1;
+};
+
+struct RCC_AHBRSTR {
+	unsigned int reserved : 17;
+	unsigned char IOPARST : 1;
+	unsigned char IOPBRST : 1;
+	unsigned char IOPCRST : 1;
+	unsigned char IOPDRST : 1;
+	unsigned char IOPERST : 1;
+	unsigned char IOPFRST : 1;
+	unsigned char reserved2 : 1;
+	unsigned char TSCRST : 1;
+	unsigned char reserved3 : 7;
+};
+
+struct RCC_CFGR2 {
+	unsigned char PREDIV : 4;
+	unsigned int reserved : 28;
+};
+
+struct RCC_CFGR3 {
+	unsigned char USART1SW : 2;
+	unsigned reserved : 2;
+	unsigned char I2C1SW : 1;
+	unsigned char I2C2SW : 1;
+	unsigned char CECSW : 1;
+	unsigned short reserved2 : 9;
+	unsigned char USART2SW : 2;
+	unsigned char USART3SW : 2;
+	unsigned short reserved3 : 12;
+};
+
+struct RCCx {
+	struct RCC_CR CR;
+	struct RCC_CFGR CFGR;
+	struct RCC_CIR CIR;
+	struct RCC_APB2RST APB2RST;
+	struct RCC_APB1RST APB1RST;
+	struct RCC_AHBENR AHBENR;
+	struct RCC_APB2ENR APB2ENR;
+	struct RCC_APB1ENR APB1ENR;
+	struct RCC_BDCR BDCR;
+	struct RCC_CSR CSR;
+	struct RCC_AHBRSTR AHBRSTR;
+	struct RCC_CFGR2 CFGR2;
+	struct RCC_CFGR3 CFGR3;
+};
+
+#define RCC ((struct RCCx*)0x40021000)
+
+#pragma endregion
 
 #endif
