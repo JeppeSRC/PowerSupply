99c8cba19038db717eb929337b5c894e5e796e7b
JeppeSRC <jeppe97rc@gmail.com>
2019-06-29 16:34:37 +0200

    Added files and shit

---
+++ MicroCode/src/core/driver/display.h
@@ -0,0 +1,6 @@
+#ifndef CORE_DRIVER_DISPLAY_H_
+#define CORE_DITVER_DISPLAY_H_
+
+void displayInit();
+
+#endif
---
+++ MicroCode/src/sys/time.h
@@ -0,0 +1,8 @@
+#ifndef SYS_TIME_H_
+#define SYS_TIME_H_
+
+#include "sys/registers/registers.h"
+
+
+
+#endif
