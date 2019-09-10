6f2a3a955a1b16c35ffd2fe3f998e9f529317841
JeppeSRC <jeppe97rc@gmail.com>
2019-09-10 15:38:10 +0200

    Updated delay timer code

--- MicroCode/src/sys/time.c
+++ MicroCode/src/sys/time.c
@@ -1,9 +1,10 @@
 #include "time.h"
 
-// Clock speed 20 Mhz
+// bus clock speed 24 Mhz
 // TIM18 reserved for delay functions
 
 void InitializeTimers() {
+	RCC->APB1ENR.TIM18EN = 1;
 	TIM18->CR1.ARPE = 0;
 	TIM18->CR1.OPM = 1;
 	TIM18->CR1.URS = 1;
@@ -14,7 +15,7 @@ void Delay(uint8 seconds) {
 }
 
 void DelayMillis(uint16 milliSeconds) {
-	TIM18->PSC = 19999; // Setting clock to 1 khz (20 Mhz / (19999 + 1)) to get one cycle every milliSecond
+	TIM18->PSC = 23999; // Setting clock to 1 khz (20 Mhz / (19999 + 1)) to get one cycle every milliSecond
 	TIM18->ARR = milliSeconds;
 	TIM18->CR1.CEN = 1;
 
@@ -22,7 +23,7 @@ void DelayMillis(uint16 milliSeconds) {
 }
 
 void DelayMicros(uint16 microSeconds) {
-	TIM18->PSC = 19; // Setting clock to 1Mhz (20 Mhz / (19+1)) to get one cycle every microSecond
+	TIM18->PSC = 23; // Setting clock to 1Mhz (20 Mhz / (19+1)) to get one cycle every microSecond
 	TIM18->ARR = microSeconds;
 	TIM18->CR1.CEN = 1;
 
