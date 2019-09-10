5377eb35daba389e569708590da1e632bf462486
JeppeSRC <jeppe97rc@gmail.com>
2019-09-10 19:59:20 +0200

    Changing how we access registers, timers done

--- MicroCode/src/sys/registers/timer.h
+++ MicroCode/src/sys/registers/timer.h
@@ -3,260 +3,147 @@
 
 #include <core/def.h>
 
-struct TIM32_CR1 {
-	uint8 CEN : 1;
-	uint8 UDIS : 1;
-	uint8 URS : 1;
-	uint8 OPM : 1;
-	uint8 DIR : 1;
-	uint8 CMS : 2;
-	uint8 ARPE : 1;
-	uint8 CKD : 2;
-	uint32 reserved : 22;
-};
+#pragma pack(push)
+#pragma pack(1)
 
-struct TIM32_CR2 {
-	uint8 reserved : 3;
-	uint8 CDDS : 1;
-	uint8 MMS : 3;
-	uint8 TI1S : 1;
-	uint32 reserved2 : 24;
-};
-
-struct TIM32_SMCR {
-	uint8 SMS : 3;
-	uint8 reserved : 1;
-	uint8 TS : 3;
-	uint8 MSM : 1;
-	uint8 ETF : 4;
-	uint8 ETPS : 2;
-	uint8 ECE : 1;
-	uint8 ETP : 1;
-	uint32 reserved2 : 16;
-};
-
-struct TIM32_DIER {
-	uint8 UIE : 1;
-	uint8 CC1IE : 1;
-	uint8 CC2IE : 1;
-	uint8 CC3IE : 1;
-	uint8 CC4IE : 1;
-	uint8 reserved : 1;
-	uint8 TIE : 1;
-	uint8 reserved2 : 1;
-	uint8 UDE : 1;
-	uint8 CC1DE : 1;
-	uint8 CC2DE : 1;
-	uint8 CC3DE : 1;
-	uint8 CC4DE : 1;
-	uint8 reserved3 : 1;
-	uint8 TDE : 1;
-	uint32 reserved4 : 17;
-};
-
-struct TIM32_SR {
-	uint8 UIF : 1;
-	uint8 CC1IF : 1;
-	uint8 CC2IF : 1;
-	uint8 CC3IF : 1;
-	uint8 CC4IF : 1;
-	uint8 reserved : 1;
-	uint8 TIF : 1;
-	uint8 reserved2 : 2;
-	uint8 CC1OF : 1;
-	uint8 CC2OF : 1;
-	uint8 CC3OF : 1;
-	uint8 CC4OF : 1;
-	uint32 reserved3 : 19;
-};
-
-struct TIM32_EGR {
-	uint8 UG : 1;
-	uint8 CC1G : 1;
-	uint8 CC2G : 1;
-	uint8 CC3G : 1;
-	uint8 CC4G : 1;
-	uint8 reserved : 1;
-	uint8 TG : 1;
-	uint32 reserved2 : 25;
-};
-
-struct TIM32_CCMR1 {
-	union {
-		struct {
-			uint8 CC1S : 2;
-			uint8 OC1FE : 1;
-			uint8 OC1PE : 1;
-			uint8 OC1M : 3;
-			uint8 OC1CE : 1;
-			uint8 CC2S : 2;
-			uint8 OC2FE : 1;
-			uint8 OC2PE : 1;
-			uint8 OC2M : 3;
-			uint8 OC2CE : 1;
-		};
-
-		struct {
-			uint8 reserved : 2;
-			uint8 IC1PSC : 2;
-			uint8 IC1F : 4;
-			uint8 reserved2 : 2;
-			uint8 IC2PSC : 2;
-			uint8 IC2F : 4;
-		};
-	};
-
-	uint16 reserved3 : 16;
-};
-
-struct TIM32_CCMR2 {
-	union {
-		struct {
-			uint8 CC3S : 2;
-			uint8 OC3FE : 1;
-			uint8 OC3PE : 1;
-			uint8 OC3M : 3;
-			uint8 OC3CE : 1;
-			uint8 CC4S : 2;
-			uint8 OC4FE : 1;
-			uint8 OC4PE : 1;
-			uint8 OC4M : 3;
-			uint8 OC4CE : 1;
-		};
-
-		struct {
-			uint8 reserved : 2;
-			uint8 IC3PSC : 2;
-			uint8 IC3F : 4;
-			uint8 reserved2 : 2;
-			uint8 IC4PSC : 2;
-			uint8 IC4F : 4;
-		};
-	};
-
-	uint16 reserved3 : 16;
-};
-
-struct TIM32_CCER {
-	uint8 CC1E : 1;
-	uint8 CC1P : 1;
-	uint8 reserved : 1;
-	uint8 CC1NP : 1;
-	uint8 CC2E : 1;
-	uint8 CC2P : 1;
-	uint8 reserved2 : 1;
-	uint8 CC2NP;
-	uint8 CC3E : 1;
-	uint8 CC3P : 1;
-	uint8 reserved3 : 1;
-	uint8 CC3NP : 1;
-	uint8 CC4E : 1;
-	uint8 CC4P : 1;
-	uint8 reserved4 : 1;
-	uint8 CC4NP : 1;
-};
-
-typedef uint32 TIM32_CNT;
-typedef uint32 TIM32_PSC;
-typedef uint32 TIM32_ARR;
-typedef uint32 TIM32_CCRx;
-
-struct TIM32_DCR {
-	uint8 DBA : 5;
-	uint8 reserved : 3;
-	uint8 DBL5 : 5;
-	uint32 reserved2 : 19;
-};
-
-struct TIM32_DMAR {
-	uint16 DMAB : 16;
-	uint16 reserved : 16;
-};
+//TIMA = 2, 3, 4, 5, 19
+#define TIMA_CR1(TIM, OP, CEN, UDIS, URS, OPM, DIR, CMS, ARPE, CKD) TIM->CR1 OP ((CKD << 8) | (ARPE << 7) | (CMS << 5) | (DIR << 4) | (OPM << 3) | (URS << 2) | (UDIS << 1) | CEN)
+#define TIM2_CR1(OP, CEN, UDIS, URS, OPM, DIR, CMS, ARPE, CKD) TIMA_CR1(TIM2, OP, CEN, UDIS, URS, OPM, DIR, CMS, ARPE, CKD)
+#define TIM3_CR1(OP, CEN, UDIS, URS, OPM, DIR, CMS, ARPE, CKD) TIMA_CR1(TIM3, OP, CEN, UDIS, URS, OPM, DIR, CMS, ARPE, CKD)
+#define TIM4_CR1(OP, CEN, UDIS, URS, OPM, DIR, CMS, ARPE, CKD) TIMA_CR1(TIM4, OP, CEN, UDIS, URS, OPM, DIR, CMS, ARPE, CKD)
+#define TIM5_CR1(OP, CEN, UDIS, URS, OPM, DIR, CMS, ARPE, CKD) TIMA_CR1(TIM5, OP, CEN, UDIS, URS, OPM, DIR, CMS, ARPE, CKD)
+#define TIM19_CR1(OP, CEN, UDIS, URS, OPM, DIR, CMS, ARPE, CKD) TIMA_CR1(TIM19, OP, CEN, UDIS, URS, OPM, DIR, CMS, ARPE, CKD)
 
 
-//Timers 2, 3, 4, 5, 19
+#define TIMA_CR2 (TIM, OP, CDDS, MMS, TI1S) TIM->CR2 OP ((TI1S << 7) | (MMS << 4) | (CDDS << 3))
+#define TIM2_CR2(OP, CDDS, MMS, TI1S) TIMA_CR2(TIM2, OP, CDDS, MMS, TI1S)
+#define TIM3_CR2(OP, CDDS, MMS, TI1S) TIMA_CR2(TIM3, OP, CDDS, MMS, TI1S)
+#define TIM4_CR2(OP, CDDS, MMS, TI1S) TIMA_CR2(TIM4, OP, CDDS, MMS, TI1S)
+#define TIM5_CR2(OP, CDDS, MMS, TI1S) TIMA_CR2(TIM5, OP, CDDS, MMS, TI1S)
+#define TIM19_CR2(OP, CDDS, MMS, TI1S) TIMA_CR2(TIM19, OP, CDDS, MMS, TI1S)
+
+
+#define TIMA_SMCR(TIM, OP, SMS, TS, MSM, ETF, ETPS, ECE, ETP) TIM->SMCR OP ((ETP << 15) | (ECE << 14) | (ETPS << 12) | (ETF << 8) | (MSM << 7) | (TS << 4) | SMS)
+#define TIM2_SMCR(OP, SMS, TS, MSM, ETF, ETPS, ECE, ETP) TIMA_SMCR(TIM2, OP, SMS, TS, MSM, ETF, ETPS, ECE, ETP)
+#define TIM3_SMCR(OP, SMS, TS, MSM, ETF, ETPS, ECE, ETP) TIMA_SMCR(TIM3, OP, SMS, TS, MSM, ETF, ETPS, ECE, ETP)
+#define TIM4_SMCR(OP, SMS, TS, MSM, ETF, ETPS, ECE, ETP) TIMA_SMCR(TIM4, OP, SMS, TS, MSM, ETF, ETPS, ECE, ETP)
+#define TIM5_SMCR(OP, SMS, TS, MSM, ETF, ETPS, ECE, ETP) TIMA_SMCR(TIM5, OP, SMS, TS, MSM, ETF, ETPS, ECE, ETP)
+#define TIM19_SMCR(OP, SMS, TS, MSM, ETF, ETPS, ECE, ETP) TIMA_SMCR(TIM19, OP, SMS, TS, MSM, ETF, ETPS, ECE, ETP)
+
+//TIMA_DIER
+
+//TIMA_EGR
+
+#define TIMA_CCMR1_OUT(TIM, OP, CC1S, OC1FE, OC1PE, OC1M, OC1CE, CC2S, OC2FE, OC2PE, OC2M, OC2CE) TIM->CCMR1 OP ((OC2CE << 15) | (OC2M << 12) | (OC2PE << 11) | (OC2FE << 10) | (CC2S << 8) | (OC1CE << 7) | (OC1M << 4) | (OC1PE << 3) | (OC1FE << 2) | CC1S)
+#define TIM2_CCMR1_OUT(OP, CC1S, OC1FE, OC1PE, OC1M, OC1CE, CC2S, OC2FE, OC2PE, OC2M, OC2CE) TIMA_CCMR1_OUT(TIM2, OP, CC1S, OC1FE, OC1PE, OC1M, OC1CE, CC2S, OC2FE, OC2PE, OC2M, OC2CE)
+#define TIM3_CCMR1_OUT(OP, CC1S, OC1FE, OC1PE, OC1M, OC1CE, CC2S, OC2FE, OC2PE, OC2M, OC2CE) TIMA_CCMR1_OUT(TIM3, OP, CC1S, OC1FE, OC1PE, OC1M, OC1CE, CC2S, OC2FE, OC2PE, OC2M, OC2CE)
+#define TIM4_CCMR1_OUT(OP, CC1S, OC1FE, OC1PE, OC1M, OC1CE, CC2S, OC2FE, OC2PE, OC2M, OC2CE) TIMA_CCMR1_OUT(TIM4, OP, CC1S, OC1FE, OC1PE, OC1M, OC1CE, CC2S, OC2FE, OC2PE, OC2M, OC2CE)
+#define TIM5_CCMR1_OUT(OP, CC1S, OC1FE, OC1PE, OC1M, OC1CE, CC2S, OC2FE, OC2PE, OC2M, OC2CE) TIMA_CCMR1_OUT(TIM5, OP, CC1S, OC1FE, OC1PE, OC1M, OC1CE, CC2S, OC2FE, OC2PE, OC2M, OC2CE)
+#define TIM19_CCMR1_OUT(OP, CC1S, OC1FE, OC1PE, OC1M, OC1CE, CC2S, OC2FE, OC2PE, OC2M, OC2CE) TIMA_CCMR1_OUT(TIM19, OP, CC1S, OC1FE, OC1PE, OC1M, OC1CE, CC2S, OC2FE, OC2PE, OC2M, OC2CE)
+
+
+#define TIMA_CCMR1_IN(TIM, OP, CC1S, IC1PSC, IC1F, CC2S, IC2PSC, IC2F) TIM->CCMR1 OP ((IC2F << 12) | (IC2PSC << 10) | (CC2S << 8) | (IC1F << 4) | (IC1PSC << 2) | CC1S)
+#define TIM2_CCMR1_IN(OP, CC1S, IC1PSC, IC1F, CC2S, IC2PSC, IC2F) TIMA_CCMR1_IN(TIM2, OP, CC1S, IC1PSC, IC1F, CC2S, IC2PSC, IC2F)
+#define TIM3_CCMR1_IN(OP, CC1S, IC1PSC, IC1F, CC2S, IC2PSC, IC2F) TIMA_CCMR1_IN(TIM3, OP, CC1S, IC1PSC, IC1F, CC2S, IC2PSC, IC2F)
+#define TIM4_CCMR1_IN(OP, CC1S, IC1PSC, IC1F, CC2S, IC2PSC, IC2F) TIMA_CCMR1_IN(TIM4, OP, CC1S, IC1PSC, IC1F, CC2S, IC2PSC, IC2F)
+#define TIM5_CCMR1_IN(OP, CC1S, IC1PSC, IC1F, CC2S, IC2PSC, IC2F) TIMA_CCMR1_IN(TIM5, OP, CC1S, IC1PSC, IC1F, CC2S, IC2PSC, IC2F)
+#define TIM19_CCMR1_IN(OP, CC1S, IC1PSC, IC1F, CC2S, IC2PSC, IC2F) TIMA_CCMR1_IN(TIM19, OP, CC1S, IC1PSC, IC1F, CC2S, IC2PSC, IC2F)
+
+
+#define TIMA_CCMR2_OUT(TIM, OP, CC3S, OC3FE, OC3PE, OC3M, OC3CE, CC4S, OC4FE, OC4PE, OC4M, OC2CE) TIM->CCMR2 OP ((OC4CE << 15) | (OC4M << 12) | (OC4PE << 11) | (OC4FE << 10) | (CC4S << 8) | (OC3CE << 7) | (OC3M << 4) | (OC3PE << 3) | (OC3FE << 2) | CC3S)
+#define TIM2_CCMR2_OUT(OP, CC3S, OC3FE, OC3PE, OC3M, OC3CE, CC4S, OC4FE, OC4PE, OC4M, OC4CE) TIMA_CCMR2_OUT(TIM2, OP, CC3S, OC3FE, OC3PE, OC3M, OC3CE, CC4S, OC4FE, OC4PE, OC4M, OC4CE)
+#define TIM3_CCMR2_OUT(OP, CC3S, OC3FE, OC3PE, OC3M, OC3CE, CC4S, OC4FE, OC4PE, OC4M, OC4CE) TIMA_CCMR2_OUT(TIM3, OP, CC3S, OC3FE, OC3PE, OC3M, OC3CE, CC4S, OC4FE, OC4PE, OC4M, OC4CE)
+#define TIM4_CCMR2_OUT(OP, CC3S, OC3FE, OC3PE, OC3M, OC3CE, CC4S, OC4FE, OC4PE, OC4M, OC4CE) TIMA_CCMR2_OUT(TIM4, OP, CC3S, OC3FE, OC3PE, OC3M, OC3CE, CC4S, OC4FE, OC4PE, OC4M, OC4CE)
+#define TIM5_CCMR2_OUT(OP, CC3S, OC3FE, OC3PE, OC3M, OC3CE, CC4S, OC4FE, OC4PE, OC4M, OC4CE) TIMA_CCMR2_OUT(TIM5, OP, CC3S, OC3FE, OC3PE, OC3M, OC3CE, CC4S, OC4FE, OC4PE, OC4M, OC4CE)
+#define TIM19_CCMR2_OUT(OP, CC3S, OC3FE, OC3PE, OC3M, OC3CE, CC4S, OC4FE, OC4PE, OC4M, OC4CE) TIMA_CCMR2_OUT(TIM19, OP, CC3S, OC3FE, OC3PE, OC3M, OC3CE, CC4S, OC4FE, OC4PE, OC4M, OC4CE)
+
+
+#define TIMA_CCMR2_IN(TIM, OP, CC3S, IC3PSC, IC3F, CC4S, IC4PSC, IC4F) TIM->CCMR2 OP (IC4F << 12) | (IC4PSC << 10) | (CC4S << 8) | (IC3F << 4) | (IC3PSC << 2) | CC3S
+#define TIM2_CCMR2_IN(OP, CC3S, IC3PSC, IC3F, CC4S, IC4PSC, IC4F) TIMA_CCMR2_IN(TIM2, OP, CC3S, IC3PSC, IC3F, CC4S, IC4PSC, IC4F)
+#define TIM3_CCMR2_IN(OP, CC3S, IC3PSC, IC3F, CC4S, IC4PSC, IC4F) TIMA_CCMR2_IN(TIM3, OP, CC3S, IC3PSC, IC3F, CC4S, IC4PSC, IC4F)
+#define TIM4_CCMR2_IN(OP, CC3S, IC3PSC, IC3F, CC4S, IC4PSC, IC4F) TIMA_CCMR2_IN(TIM4, OP, CC3S, IC3PSC, IC3F, CC4S, IC4PSC, IC4F)
+#define TIM5_CCMR2_IN(OP, CC3S, IC3PSC, IC3F, CC4S, IC4PSC, IC4F) TIMA_CCMR2_IN(TIM5, OP, CC3S, IC3PSC, IC3F, CC4S, IC4PSC, IC4F)
+#define TIM19_CCMR2_IN(OP, CC3S, IC3PSC, IC3F, CC4S, IC4PSC, IC4F) TIMA_CCMR2_IN(TIM19, OP, CC3S, IC3PSC, IC3F, CC4S, IC4PSC, IC4F)
+
+
+#define TIMA_CCER(TIM, OP, CC1E, CC1P, CC1NP, CC2E, CC2P, CC2NP, CC3E, CC3P, CC3NP, CC4E, CC4P, CC4NP) TIM->CCER OP ((CC4NP << 15) | (CC4P << 13) | (CC4E << 12) | (CC3NP << 11) | (CC3P << 9) | (CC3E << 8) | (CC2NP << 7) | (CC2P << 5) | (CC2E << 4) | (CC1NP << 3) | (CC1P << 1) | CC1E)
+#define TIM2_CCER(OP, CC1E, CC1P, CC1NP, CC2E, CC2P, CC2NP, CC3E, CC3P, CC3NP, CC4E, CC4P, CC4NP) TIMA_CCER(TIM2, OP, CC1E, CC1P, CC1NP, CC2E, CC2P, CC2NP, CC3E, CC3P, CC3NP, CC4E, CC4P, CC4NP)
+#define TIM3_CCER(OP, CC1E, CC1P, CC1NP, CC2E, CC2P, CC2NP, CC3E, CC3P, CC3NP, CC4E, CC4P, CC4NP) TIMA_CCER(TIM3, OP, CC1E, CC1P, CC1NP, CC2E, CC2P, CC2NP, CC3E, CC3P, CC3NP, CC4E, CC4P, CC4NP)
+#define TIM4_CCER(OP, CC1E, CC1P, CC1NP, CC2E, CC2P, CC2NP, CC3E, CC3P, CC3NP, CC4E, CC4P, CC4NP) TIMA_CCER(TIM4, OP, CC1E, CC1P, CC1NP, CC2E, CC2P, CC2NP, CC3E, CC3P, CC3NP, CC4E, CC4P, CC4NP)
+#define TIM5_CCER(OP, CC1E, CC1P, CC1NP, CC2E, CC2P, CC2NP, CC3E, CC3P, CC3NP, CC4E, CC4P, CC4NP) TIMA_CCER(TIM5, OP, CC1E, CC1P, CC1NP, CC2E, CC2P, CC2NP, CC3E, CC3P, CC3NP, CC4E, CC4P, CC4NP)
+#define TIM19_CCER(OP, CC1E, CC1P, CC1NP, CC2E, CC2P, CC2NP, CC3E, CC3P, CC3NP, CC4E, CC4P, CC4NP) TIMA_CCER(TIM19, OP, CC1E, CC1P, CC1NP, CC2E, CC2P, CC2NP, CC3E, CC3P, CC3NP, CC4E, CC4P, CC4NP)
+
 struct TIMxA {
-	struct TIM32_CR1 CR1;
-	struct TIM32_CR2 CR2;
-	struct TIM32_SMCR SMCR;
-	struct TIM32_DIER DIER;
-	struct TIM32_SR SR;
-	struct TIM32_EGR EGR;
-	struct TIM32_CCMR1 CCMR1;
-	struct TIM32_CCMR2 CCMR2;
-	struct TIM32_CCER CCER;
-	TIM32_CNT CNT;
-	TIM32_PSC PSC;
-	TIM32_ARR ARR;
+	uint32 CR1;
+	uint32 CR2;
+	uint32 SMCR;
+	uint32 DIER;
+	uint32 SR;
+	uint32 EGR;
+	uint32 CCMR1;
+	uint32 CCMR2;
+	uint32 CCER;
+	uint32 CNT;
+	uint32 PSC;
+	uint32 ARR;
 	uint32 reserved : 32;
-	TIM32_CCRx CCR1;
-	TIM32_CCRx CCR2;
-	TIM32_CCRx CCR3;
-	TIM32_CCRx CCR4;
+	uint32 CCR1;
+	uint32 CCR2;
+	uint32 CCR3;
+	uint32 CCR4;
 	uint32 reserved2 : 32;
-	struct TIM32_DCR DCR;
-	struct TIM32_DMAR DMAR;
+	uint32 DCR;
+	uint32 DMAR;
 };
 
-struct TIM16_CR1 {
-	uint8 CEN : 1;
-	uint8 UDIS : 1;
-	uint8 URS : 1;
-	uint8 OPM : 1;
-	uint8 reserved : 3;
-	uint8 ARPE : 1;
-	uint8 reserved2 : 8;
-	uint16 reserved3 : 16;
-};
+//TIMB = 6, 7, 18
 
-struct TIM16_CR2 {
-	uint8 reserved : 4;
-	uint8 MMS : 3;
-	uint16 reserved2 : 9;
-	uint16 reserved3 : 16;
-};
+#define TIMB_CR1(TIM, OP, CEN, UDIS, URS, OPM, ARPE) TIM->CR1 OP ((ARPE << 7) | (OPM << 3) | (URS << 2) | (UDIS << 1) | CEN)
+#define TIM6_CR1(OP, CEN, UDIS, URS, OPM, ARPE) TIMB_CR1(TIM6, OP, CEN, UDIS, URS, OPM, ARPE)
+#define TIM7_CR1(OP, CEN, UDIS, URS, OPM, ARPE) TIMB_CR1(TIM7, OP, CEN, UDIS, URS, OPM, ARPE)
+#define TIM18_CR1(OP, CEN, UDIS, URS, OPM, ARPE) TIMB_CR1(TIM18, OP, CEN, UDIS, URS, OPM, ARPE)
 
-struct TIM16_DIER {
-	uint8 UIE : 1;
-	uint8 reserved : 7;
-	uint8 UDE : 1;
-	uint8 reserved2 : 7;
-	uint16 reserved3 : 16;
-};
 
-struct TIM16_SR {
-	uint8 UIF : 1;
-	uint16 reserved : 15;
-	uint16 reserved2 : 16;
-};
+#define TIMB_CR2(TIM, OP, MMS) TIM->CR2 OP (MMS << 4)
+#define TIM6_CR2(OP, MMS) TIMB_CR2(TIM6, OP, MMS)
+#define TIM7_CR2(OP, MMS) TIMB_CR2(TIM7, OP, MMS)
+#define TIM18_CR2(OP, MMS) TIMB_CR2(TIM18, OP, MMS)
 
-struct TIM16_EGR {
-	uint8 UG : 1;
-	uint16 reserved : 15;
-	uint16 reserved2 : 16;
-};
 
-typedef uint16 TIM16_CNT;
-typedef uint16 TIM16_PSC;
-typedef uint16 TIM16_ARR;
+#define TIMB_DIER(TIM, OP, UIE, UDE) TIM->DIER OP ((UDE << 8) | UIE)
+#define TIM6_DIER(OP, UIE, UDE) TIMB_DIER(TIM6, OP, UIE, UDE)
+#define TIM7_DIER(OP, UIE, UDE) TIMB_DIER(TIM7, OP, UIE, UDE)
+#define TIM18_DIER(OP, UIE, UDE) TIMB_DIER(TIM18, OP, UIE, UDE)
 
-// Timers 6, 7, 18
+
+#define TIMB_SR(TIM, OP, UIF) TIM->SR OP (UIF)
+#define TIM6_SR(OP, UIF) TIMB_SR(TIM6, OP, UIF)
+#define TIM7_SR(OP, UIF) TIMB_SR(TIM7, OP, UIF)
+#define TIM18_SR(OP, UIF) TIMB_SR(TIM18, OP, UIF)
+
+
+#define TIMB_EGR(TIM, OP, UG) TIM->EGR OP (UG)
+#define TIM6_EGR(OP, UG) TIMB_EGR(TIM6, OP, UG)
+#define TIM7_EGR(OP, UG) TIMB_EGR(TIM7, OP, UG)
+#define TIM18_EGR(OP, UG) TIMB_EGR(TIM18, OP, UG)
+
 struct TIMxB {
-	struct TIM16_CR1 CR1;
-	struct TIM16_CR2 CR2;
+	uint32 CR1;
+	uint32 CR2;
 	uint32 reserved;
-	struct TIM16_DIER DIER;
-	struct TIM16_SR SR;
-	struct TIM16_EGR EGR;
+	uint32 DIER;
+	uint32 SR;
+	uint32 EGR;
 	uint32 res;
 	uint32 res1;
 	uint32 res2;
-	TIM16_CNT CNT;
+	uint16 CNT;
 	uint16 reserved3;
-	TIM16_PSC PSC;
+	uint16 PSC;
 	uint16 reserved4;
-	TIM16_ARR ARR;
+	uint16 ARR;
 	uint16 reserved5;
 };
 
+#pragma pack(pop)
 #endif
