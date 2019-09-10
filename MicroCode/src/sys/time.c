d86846b843f267179c98e4505cf15e04c113bebd
JeppeSRC <jeppe97rc@gmail.com>
2019-09-10 20:01:06 +0200

    Updated time.c to use the new system

--- MicroCode/src/sys/time.c
+++ MicroCode/src/sys/time.c
@@ -3,19 +3,16 @@
 // bus clock speed 24 Mhz
 // TIM18 reserved for delay functions
 // TIM2 reserved for time tracking
+#include <memory.h>
 
 void InitializeTimers() {
 	RCC->APB1ENR.TIM18EN = 1;
-	TIM18->CR1.ARPE = 0;
-	TIM18->CR1.OPM = 1;
-	TIM18->CR1.URS = 1;
+	TIM18_CR1(=, 0, 0, 1, 1, 0);
 
 	RCC->APB1ENR.TIM2EN = 1;
 	TIM2->ARR = ~0;
 	TIM2->PSC = 23;
-	TIM2->CR1.URS = 1;
-	TIM2->CR1.UDIS = 1;
-	TIM2->CR1.CEN = 1;
+	TIM2_CR1(=, 1, 1, 1, 0, 0, 0, 0, 0);
 }
 
 void ResetTimers() {
@@ -29,7 +26,7 @@ void Delay(uint8 seconds) {
 void DelayMillis(uint16 milliSeconds) {
 	TIM18->PSC = 23999; // Setting clock to 1 khz (20 Mhz / (19999 + 1)) to get one cycle every milliSecond
 	TIM18->ARR = milliSeconds;
-	TIM18->CR1.CEN = 1;
+	TIM18_CR1(| , 1, 0, 0, 0, 0);
 
 	asm("wfe");
 }
@@ -37,15 +34,15 @@ void DelayMillis(uint16 milliSeconds) {
 void DelayMicros(uint16 microSeconds) {
 	TIM18->PSC = 23; // Setting clock to 1Mhz (20 Mhz / (19+1)) to get one cycle every microSecond
 	TIM18->ARR = microSeconds;
-	TIM18->CR1.CEN = 1;
+	TIM18_CR1(| , 1, 0, 0, 0, 0);
 
 	asm("wfe");
 }
 
-void Millis() {
+uint32 Millis() {
 	return TIM2->CNT / 1000;
 }
 
-void Micros() {
+uint32 Micros() {
 	return TIM2->CNT;
 }
