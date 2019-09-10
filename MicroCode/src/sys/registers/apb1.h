40ac0ddc718e35d466fd80e9e70741dfcd91fa42
JeppeSRC <jeppe97rc@gmail.com>
2019-09-10 21:32:29 +0200

    Rewrite DAC register code to new system

--- MicroCode/src/sys/registers/apb1.h
+++ MicroCode/src/sys/registers/apb1.h
@@ -5,6 +5,9 @@
 
 #include "timer.h"
 
+#pragma pack(push)
+#pragma pack(1)
+
 #pragma region PWR
 
 struct PWR_CR {
@@ -43,118 +46,31 @@ struct PWRx {
 
 #pragma region DAC
 
-struct DAC_CR {
-	uint8 EN1 : 1;
-	uint8 BOFF1 : 1;
-	uint8 TEN1 : 1;
-	uint8 TSEL1 : 3;
-	uint8 WAVE1 : 2;
-	uint8 MAMP1 : 4;
-	uint8 DMAEN1 : 1;
-	uint8 DMAUDRIE1 : 1;
-	uint8 reserved : 2;
-	uint8 EN2 : 1;
-	uint8 BOFF2 : 1;
-	uint8 TEN2 : 1;
-	uint8 TSEL2 : 3;
-	uint8 WAVE2 : 2;
-	uint8 MAMP2 : 4;
-	uint8 DMAEN2 : 1;
-	uint8 DMAUDRIE2 : 1;
-	uint8 reserved2 : 2;
-};
+#define DAC_CR(DAC, OP, EN1, BOFF1, TEN1, TSEL1, WAVE1, MAMP1, DMAEN1, DMAUDRIE1, EN2, BOFF2, TEN2, TSEL2, WAVE2, MAMP2, DMAEN2, DMAUDRIE2) DAC->CR OP ((DMAUDRUE2 << 29) | (DMAEN2 << 28) | (MAMP2 << 24) | (WAVE2 << 22) | (TSEL2 << 19) | (TEN2 << 18) | (BOFF2 << 17) | (EN2 << 16) | (DMAUDRIE1 << 13) | (DMAEN1 << 12) | (MAMP1 << 8) | (WAVE1 << 6) | (TSEL1 << 3) | (TEN1 << 2) | (BOFF1 << 1) | EN1)
+#define DAC1_CR(OP, EN1, BOFF1, TEN1, TSEL1, WAVE1, MAMP1, DMAEN1, DMAUDRIE1, EN2, BOFF2, TEN2, TSEL2, WAVE2, MAMP2, DMAEN2, DMAUDRIE2) DAC_CR(DAC1, OP, EN1, BOFF1, TEN1, TSEL1, WAVE1, MAMP1, DMAEN1, DMAUDRIE1, EN2, BOFF2, TEN2, TSEL2, WAVE2, MAMP2, DMAEN2, DMAUDRIE2)
+#define DAC2_CR(OP, EN1, BOFF1, TEN1, TSEL1, WAVE1, MAMP1, DMAEN1, DMAUDRIE1, EN2, BOFF2, TEN2, TSEL2, WAVE2, MAMP2, DMAEN2, DMAUDRIE2) DAC_CR(DAC2, OP, EN1, BOFF1, TEN1, TSEL1, WAVE1, MAMP1, DMAEN1, DMAUDRIE1, EN2, BOFF2, TEN2, TSEL2, WAVE2, MAMP2, DMAEN2, DMAUDRIE2)
 
-struct DAC_SWTRIGR {
-	uint8 SWTRIG1 : 1;
-	uint8 SWTRIG2 : 1;
-	uint32 reserved : 30;
-};
 
-struct DAC_DHR12R1 {
-	uint16 DACC1DHR : 12;
-	uint32 reserved : 20;
-};
+#define DAC_SWTRIGR(DAC, OP, SWTRIG1, SWTRIG2) DAC->SWTRIGR OP ((SWTRIG2 << 1) | SWTRIG1)
+#define DAC1_SWTRIGR(OP, SWTRIG1, SWTRIG2) DAC_SWTRIGR(DAC1, OP, SWTRIG1, SWTRIG2)
+#define DAC2_SWTRIGR(OP, SWTRIG1, SWTRIG2) DAC_SWTRIGR(DAC2, OP, SWTRIG1, SWTRIG2)
 
-struct DAC_DHR12L1 {
-	uint8 reserved : 4;
-	uint16 DACC1DHR : 12;
-	uint16 reserved2 : 16;
-};
-
-struct DAC_DHR8R1 {
-	uint16 DACC1DHR : 8;
-	uint32 reserved : 24;
-};
-
-struct DAC_DHR12R2 {
-	uint16 DACC2DHR : 12;
-	uint32 reserved : 20;
-};
-
-struct DAC_DHR12L2 {
-	uint8 reserved : 4;
-	uint16 DACC2DHR : 12;
-	uint16 reserved2 : 16;
-};
-
-struct DAC_DHR8R2 {
-	uint16 DACC2DHR : 8;
-	uint32 reserved : 24;
-};
-
-struct DAC_DHR12RD {
-	uint16 DACC1DHR : 12;
-	uint8 reserved : 4;
-	uint16 DACC2DHR : 12;
-	uint8 reserved2 : 4;
-};
-
-struct DAC_DHR12LD {
-	uint8 reserved : 4;
-	uint16 DACC1DHR : 12;
-	uint8 reserved2 : 4;
-	uint16 DACC2DHR : 12;
-};
-
-struct DAC_DHR8RD {
-	uint8 DACC1DHR : 8;
-	uint8 DACC2DHR : 8;
-	uint16 reserved : 16;
-};
-
-struct DAC_DOR1 {
-	uint16 DACC1DOR : 12;
-	uint32 reserved : 20;
-};
-
-struct DAC_DOR2 {
-	uint16 DACC2DOR : 12;
-	uint32 reserved : 20;
-};
-
-struct DAC_SR {
-	uint16 reserved : 13;
-	uint8 DMAUDR1 : 1;
-	uint32 reserved2 : 15;
-	uint8 DMAUDR2 : 1;
-	uint8 reserved3 : 2;
-};
 
 struct DACx {
-	struct DAC_CR CR;
-	struct DAC_SWTRIGR SWTRIGR;
-	struct DAC_DHR12R1 DHR12R1;
-	struct DAC_DHR12L1 DHR12L1;
-	struct DAC_DHR8R1 DHR8R1;
-	struct DAC_DHR12R2 DHR12R2;
-	struct DAC_DHR12L2 DHR12L2;
-	struct DAC_DHR8R2 DHR8R2;
-	struct DAC_DHR12RD DHR12RD;
-	struct DAC_DHR12LD DHR12LD;
-	struct DAC_DHR8RD DHR8RD;
-	struct DAC_DOR1 DOR1;
-	struct DAC_DOR2 DOR2;
-	struct DAC_SR SR;
+	uint32 CR;
+	uint32 SWTRIGR;
+	uint32 DHR12R1;
+	uint32 DHR12L1;
+	uint32 DHR8R1;
+	uint32 DHR12R2;
+	uint32 DHR12L2;
+	uint32 DHR8R2;
+	uint32 DHR12RD;
+	uint32 DHR12LD;
+	uint32 DHR8RD;
+	uint32 DOR1;
+	uint32 DOR2;
+	uint32 SR;
 };
 
 #define DAC1 ((struct DACx*)0x40007400)
@@ -175,3 +91,5 @@ struct DACx {
 #pragma endregion
 
 #endif
+
+#pragma pack(pop)
