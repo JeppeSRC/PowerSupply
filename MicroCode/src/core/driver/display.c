5a8d6a00bb2224fb028bf636415e431e0556c931
JeppeSRC <jeppe97rc@gmail.com>
2019-09-11 21:26:59 +0200

    ExecuteCommand function for display driver

--- MicroCode/src/core/driver/display.c
+++ MicroCode/src/core/driver/display.c
@@ -3,107 +3,72 @@
 #include <core/string.h>
 
 /*	Device			Port			PhysicalPin
-	Display_RS		PB0				18
-	Display_RW		PB1				19
-	Display_E		PB2				20
-	Display_D0		PB3				39
-	Display_D1		PB4				40
-	Display_D2		PB5				41
-	Display_D3		PB6				42
-	Display_D4		PB7				43
-	Display_D5		PB8				45
-	Display_D6		PB9				46
-	Display_D7		PB14			26
+	Display_RS		PA13			34
+	Display_RW		PF6				35
+	Display_E		PF7				36
+	Display_D4		PA14			37
+	Display_D5		PA15			38
+	Display_D6		PB3				39
+	Display_D7		PB4				40
  */
 
+#define ODRA_MASK 0x1FFFFFFF 
+#define ODRB_MASK 0xFFFFFFE7
+#define ODRF_MASK 0xFFFFFF3F
+
+/*instCode format:
+	0:7	instruction
+	8	RW
+	9	RS
+*/
+
+void ExecuteCommand(uint32 instCode) {
+	uint32 odra = GPIOA->ODR & ODRA_MASK;
+	uint32 odrb = GPIOB->ODR & ODRB_MASK;
+	uint32 odrf = GPIOA->ODR & ODRF_MASK;
+
+	//Sets RS, RW and high nibble of instruction
+	GPIOA->ODR = (odra |= ((instCode & 0x200) << 4)) | ((instCode & 0x30) << 10); //Sets RS, D4 and D5
+	GPIOB->ODR = odrb | ((instCode & 0xC0) >> 3); //Sets D6 and D7
+	GPIOF->ODR = odrf | ((instCode & 0x100) >> 2); //Sets RW
+
+	//Toggle E
+	GPIOF->ODR ^= 0x80;
+	DelayMicros(1);
+	GPIOF->ODR ^= 0x80;
+	
+	//Sets low nibble of instruction
+	GPIOA->ODR = odra | ((instCode & 0x3) << 14); //Sets D4 and D5
+	GPIOB->ODR = odrb | ((instCode & 0xC) << 1); //Sets D6 and D7
+
+	//Toggle E
+	GPIOF->ODR ^= 0x80;
+	DelayMicros(1);
+	GPIOF->ODR ^= 0x80;
+}
 
 void WaitBusy() {
-	GPIOB->MODER.p14 = 0;
-
-	struct GPIOx_ODR* odr = &GPIOB->ODR;
-
-	odr->p = 0x2;
-
-	do {
-		DelayMicros(10);
-
-		odr->p2 = 1;
-		DelayMicros(1);
-	} while (GPIOB->IDR.p14);
-
-	GPIOB->MODER.p14 = 1;
+	
 }
 
 void ClearDisplay() {
-	//WaitBusy();
 
-	GPIOB->ODR.p = 0x8;
-	GPIOB->ODR.p2 = 1;
-	DelayMicros(1);
-	GPIOB->ODR.p2 = 0;
-	DelayMillis(2);
 }
 
 void ReturnHome() {
-	//WaitBusy();
 
-	GPIOB->ODR.p = 0x10;
-	GPIOB->ODR.p2 = 1;
-	DelayMicros(1);
-	GPIOB->ODR.p2 = 0;
-	DelayMillis(2);
 }
 
 void SetDDRAMAddress(uint8 address) {
-	//WaitBusy();
 
-	GPIOB->ODR.p = ((uint16)address & 0x7F) << 3;
-	GPIOB->ODR.p14 = 1;
-	
-	GPIOB->ODR.p2 = 1;
-	DelayMicros(1);
-	GPIOB->ODR.p2 = 0;
-	DelayMicros(50);
 }
 
 void Print(const char* string) {
-	uint32 len = strlen(string);
 
-	for (uint32 i = 0; i < len; i++) {
-		uint16 data = (uint16)(string[i]) << 3;
-		data |= ((data & 0x400) >> 4) | 1;
-
-		GPIOB->ODR.p = data;
-		DelayMicros(1);
-		GPIOB->ODR.p2 = 0;
-		DelayMicros(50);
-	}
 }
 
 void InitializeDisplay() {
-	DelayMillis(100);
-	//WaitBusy();
 
-	// Function set
-	GPIOB->ODR.p = 0b111100000;
-	GPIOB->ODR.p2 = 1;
-	DelayMicros(1);
-	GPIOB->ODR.p2 = 0;
-	DelayMicros(50);
-
-	// Entry mode set
-	GPIOB->ODR.p = 0b10000;
-	GPIOB->ODR.p2 = 1;
-	DelayMicros(1);
-	GPIOB->ODR.p2 = 0;
-	DelayMicros(50);
-
-	// Display on/off
-	GPIOB->ODR.p = 0b1100000;
-	GPIOB->ODR.p2 = 1;
-	DelayMicros(1);
-	GPIOB->ODR.p2 = 0;
-	DelayMicros(50);
 }
 
 void DisplayPrint(uint8 address, const char* string) {
