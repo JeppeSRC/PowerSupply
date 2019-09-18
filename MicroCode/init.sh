f35864d256f2b575cb47acbc8c2504af806a17e4
JeppeSRC <jeppe97rc@gmail.com>
2019-09-18 16:37:44 +0200

    Updated elf2dfuse version

--- MicroCode/init.sh
+++ MicroCode/init.sh
@@ -1,5 +1,5 @@
 mkdir tools
 touch tools/elf2dfuse.exe
 touch tools/FuckMake.exe
-curl https://github.com/JeppeSRC/elf2dfuse/releases/download/v1/elf2dfuse.exe -L -o tools/elf2dfuse.exe
+curl https://github.com/JeppeSRC/elf2dfuse/releases/download/v1.1/elf2dfuse.exe -L -o tools/elf2dfuse.exe
 curl https://github.com/JeppeSRC/FuckMake/releases/download/v1/FuckMake.exe -L -o tools/FuckMake.exe
