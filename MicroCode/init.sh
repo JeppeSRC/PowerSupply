5b12428ce9d33aabf360c7c6be83e0cccd46d5bd
JeppeSRC <jeppe97rc@gmail.com>
2019-09-18 16:14:07 +0200

    Init script that downloads fuckmake and elf2defuse

---
+++ MicroCode/init.sh
@@ -0,0 +1,5 @@
+mkdir tools
+touch tools/elf2dfuse.exe
+touch tools/FuckMake.exe
+curl https://github.com/JeppeSRC/elf2dfuse/releases/download/v1/elf2dfuse.exe -L -o tools/elf2dfuse.exe
+curl https://github.com/JeppeSRC/FuckMake/releases/download/v1/FuckMake.exe -L -o tools/FuckMake.exe
