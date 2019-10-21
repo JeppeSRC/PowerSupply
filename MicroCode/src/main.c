5454734d7c53bab138c7fa7d28622b5acda98b39
JeppeSRC <theholyhorse.dev@gmail.com>
2019-10-21 19:00:24 +0200

    Stuff

--- MicroCode/src/main.c
+++ MicroCode/src/main.c
@@ -1,11 +1,14 @@
 #include <sys/setup.h>
 
-void EXTI0_Handler() { //Encoder 0 (Voltage)
+volatile unsigned int vSet = 0;
+volatile unsigned int cSet = 0;
 
+void EXTI0_Handler() { //Encoder 0 (Voltage)
+	vSet += (GPIOA_IDR(&, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0) >> 1);
 }
 
 void EXTI5_9_Handler() { //Encoder 1 (Current)
-
+	cSet += (GPIOB_IDR(&, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0) >> 1);
 }
 
 int main() {
