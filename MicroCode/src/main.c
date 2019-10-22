58cfc538bde9f2397c719eab13482efa08325f60
JeppeSRC <theholyhorse.dev@gmail.com>
2019-10-22 20:56:45 +0200

    Update interrupt handlers

--- MicroCode/src/main.c
+++ MicroCode/src/main.c
@@ -4,11 +4,11 @@ volatile unsigned int vSet = 0;
 volatile unsigned int cSet = 0;
 
 void EXTI0_Handler() { //Encoder 0 (Voltage)
-	vSet += (GPIOA_IDR(&, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0) >> 1);
+	vSet += (GPIOA_IDR & IDR(1, 1)) >> 1;
 }
 
 void EXTI5_9_Handler() { //Encoder 1 (Current)
-	cSet += (GPIOB_IDR(&, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0) >> 1);
+	cSet += (GPIOB_IDR & IDR(7, 1)) >> 7;
 }
 
 int main() {
