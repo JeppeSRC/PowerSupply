e676d517be8c1504f71c280eb2ece39bebc06918
JeppeSRC <theholyhorse.dev@gmail.com>
2019-10-24 21:07:57 +0200

    Added interrupt handlers

--- MicroCode/src/main.c
+++ MicroCode/src/main.c
@@ -11,8 +11,21 @@ void EXTI5_9_Handler() { //Encoder 1 (Current)
 	cSet += (GPIOB_IDR & IDR(7, 1)) >> 7;
 }
 
+void EXTI2TS_Handler() { // Switch (Encoder 0)
+
+}
+
+void EXTI3_Handler() { //Swicth (Encoder 1)
+
+}
+
+volatile uint8 outputOn = 0;
+
 int main() {
 	Initialize();
 	
+	while (1) {
+	
+	}
 
 }
