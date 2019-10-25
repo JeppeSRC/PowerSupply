171b7638a5d910cbf9a9a428a9850d5babf2d24d
JeppeSRC <theholyhorse.dev@gmail.com>
2019-10-25 19:07:18 +0200

    Added Fault handlers that just blink a led

---
+++ MicroCode/src/sys/error.c
@@ -0,0 +1,33 @@
+#include <core/def.h>
+#include <sys/registers/registers.h>
+#include <sys/time.h>
+
+void Error(uint16 period) {
+	while (1) {
+		DelayMillis(period >> 1);
+		GPIOB_ODR ^= ODR(9, 1);
+		DelayMillis(period >> 1);
+		GPIOB_ODR ^= ODR(9, 1);
+	}
+}
+
+void NMI_Handler() {
+	Error(500);
+}
+
+void HardFault_Handler() {
+	Error(1000);
+}
+
+
+void MMFault_Handler() {
+	Error(1500);
+}
+
+void BusFault_Handler() {
+	Error(2000);
+}
+
+void UsageFault_Handler() {
+	Error(2500);
+}
