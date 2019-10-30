b6ba9336da204cc3657a2b089e8e917ae970e14d
JeppeSRC <theholyhorse.dev@gmail.com>
2019-10-30 18:19:03 +0100

    Rewrite ahb1

--- MicroCode/src/sys/registers/ahb1.h
+++ MicroCode/src/sys/registers/ahb1.h
@@ -3,272 +3,202 @@
 
 #include <core/def.h>
 
-#pragma pack(push)
-#pragma pack(1)
-
 #pragma region RCC
 
-struct RCC_CR {
-	uint8 HSION : 1;
-	uint8 HSIRDY : 1;
-	uint8 reserved : 1;
-	uint8 HSITRIM : 5;
-	uint8 HSICAL : 8;
-	uint8 HSEON : 1;
-	uint8 HSERDY : 1;
-	uint8 HSEBYP : 1;
-	uint8 CSSON : 1;
-	uint8 reserved1 : 4;
-	uint8 PLLON : 1;
-	uint8 PLLRDY : 1;
-	uint8 reserved2 : 6;
-};
+#define RCC 0x40021000
 
-struct RCC_CFGR {
-	uint8 SW : 2;
-	uint8 SWS : 2;
-	uint8 HPRE : 4;
-	uint8 PPRE1 : 3;
-	uint8 PPRE2 : 3;
-	uint8 ADCPRE : 2;
-	uint8 PLLSRC : 1;
-	uint8 PLLXTPRE : 1;
-	uint8 PLLMUL : 4;
-	uint8 USBPRE : 1;
-	uint8 reserved : 1;
-	uint8 MCO : 3;
-	uint8 SDPRE : 5;
-};
+#define RCC_CR (*(uint32*)(RCC))
 
-struct RCC_CIR {
-	uint8 LSIRDYF : 1;
-	uint8 LSERDYF : 1;
-	uint8 HSIRDYF : 1;
-	uint8 HSERDYF : 1;
-	uint8 PLLRDYF : 1;
-	uint8 reserved : 2;
-	uint8 CSSF : 1;
-	uint8 LSIRDYIE : 1;
-	uint8 LSERDYIE : 1;
-	uint8 HSIRDYIE : 1;
-	uint8 HSERDYIE : 1;
-	uint8 PLLRDYIE : 1;
-	uint8 reserved1 : 3;
-	uint8 LSIRDYC : 1;
-	uint8 LSERDYC : 1;
-	uint8 HSIRDYC : 1;
-	uint8 HSERDYC : 1;
-	uint8 PLLRDYC : 1;
-	uint8 reserved2 : 2;
-	uint8 CSSC : 1;
-	uint8 reserved3 : 8;
-};
+#define PLLRDY 0x2000000
+#define PLLON 0x1000000
+#define CSSON 0x80000
+#define HSEBYP 0x40000
+#define HSERDY 0x20000
+#define HSEON 0x10000
+#define HSICAL(cal) ((cal & 0xFF) << 8)
+#define HSITRIM(trim) ((trim & 0x1F) << 3)
+#define HSIRDY 0x2
+#define HSION 0x1
 
-struct RCC_APB2RSTR {
-	uint8 SYSCFGRST : 1;
-	uint8 reserved : 8;
-	uint8 ADCRST : 1;
-	uint8 reserved1 : 2;
-	uint8 SPI1RST : 1;
-	uint8 reserved2 : 1;
-	uint8 USART1RST : 1;
-	uint8 reserved3 : 1;
-	uint8 TIM15RST : 1;
-	uint8 TIM16RST : 1;
-	uint8 TIM17RST : 1;
-	uint8 TIM19RST : 1;
-	uint8 reserved4 : 4;
-	uint8 SDAD1RST : 1;
-	uint8 SDAD2RST : 1;
-	uint8 SDAD3RST : 1;
-	uint8 reserved5 : 5;
-};
+#define RCC_CFGR (*(uint32*)(RCC+0x04))
 
-struct RCC_APB1RSTR {
-	uint8 TIM2RST : 1;
-	uint8 TIM3RST : 1;
-	uint8 TIM4RST : 1;
-	uint8 TIM5RST : 1;
-	uint8 TIM6RST : 1;
-	uint8 TIM7RST : 1;
-	uint8 TIM12RST : 1;
-	uint8 TIM13RST : 1;
-	uint8 TIM14RST : 1;
-	uint8 TIM18RST : 1;
-	uint8 reserved : 1;
-	uint8 WWDGRST : 1;
-	uint8 reserved2 : 2;
-	uint8 SPI2RST : 1;
-	uint8 SPI3RST : 1;
-	uint8 reserved3 : 1;
-	uint8 USART2RST : 1;
-	uint8 USART3RST : 1;
-	uint8 reserved4 : 2;
-	uint8 I2C1RST : 1;
-	uint8 I2C2RST : 1;
-	uint8 USBRST : 1;
-	uint8 reserved5 : 1;
-	uint8 CANRST : 1;
-	uint8 DAC2RST : 1;
-	uint8 reserved6 : 1;
-	uint8 PWRRST : 1;
-	uint8 DAC1RST : 1;
-	uint8 CECRST : 1;
-	uint8 reserved7 : 1;
-};
+#define SDPRE(sdpre) ((sdpre & 0x1F) << 27)
+#define MCO(mco) ((mco & 0x7) << 24)
+#define USBPRE 0x400000
+#define PLLMUL(mul) ((mul & 0xF) << 18)
+#define PLLXTPRE 0x20000
+#define PLLSRC 0x10000
+#define ADCPRE(pre) ((pre & 0x3) << 14)
+#define PPRE2(pre) ((pre & 0x7) << 8)
+#define PPRE1(pre) ((pre & 0xF) << 4)
+#define HPRE(pre) ((pre & 0x7) << 4)
+#define SWS(sws) ((sws & 0x3) << 2)
+#define SW(sw) ((sw & 0x3))
 
-struct RCC_AHBENR {
-	uint8 DMAEN : 1;
-	uint8 DMA2EN : 1;
-	uint8 SRAMEN : 1;
-	uint8 reserved : 1;
-	uint8 FLITFEN : 1;
-	uint8 reserved2 : 1;
-	uint8 CRCEN : 1;
-	uint16 reserved3 : 10;
-	uint8 IOPAEN : 1;
-	uint8 IOPBEN : 1;
-	uint8 IOPCEN : 1;
-	uint8 IOPDEN : 1;
-	uint8 IOPEEN : 1;
-	uint8 IOPFEN : 1;
-	uint8 reserved4 : 1;
-	uint8 TSCEN : 1;
-	uint8 reserved5 : 7;
+#define RCC_CIR (*(uint32*)(RCC+0x08))
 
-};
+#define CSSC 0x800000
+#define PLLRDYC 0x100000
+#define HSERDYC 0x80000
+#define HSIRDYC 0x40000
+#define LSERDYC 0x20000
+#define LSIRDYC 0x10000
+#define PLLRDYIE 0x1000
+#define HSERDYIE 0x800
+#define HSIRDYIE 0x400
+#define LSERDYIE 0x200
+#define LSIRDYIE 0x100
+#define CSSF 0x80
+#define PLLRDYF 0x10
+#define HSERDYF 0x8
+#define HSIRDYF 0x4
+#define LSERDYF 0x2
+#define LSIRDYF 0x1
 
-struct RCC_APB2ENR {
-	uint8 SYSCFGEN : 1;
-	uint8 reserved : 8;
-	uint8 ADCEN : 1;
-	uint8 reserved2 : 2;
-	uint8 SPI1EN : 1;
-	uint8 reserved3 : 1;
-	uint8 USART1EN : 1;
-	uint8 reserved4 : 1;
-	uint8 TIM15EN : 1;
-	uint8 TIM16EN : 1;
-	uint8 TIM17EN : 1;
-	uint8 TIM19EN : 1;
-	uint8 reserved5 : 2;
-	uint8 DBGMCUEN : 1;
-	uint8 reserved6 : 1;
-	uint8 SDADC1EN : 1;
-	uint8 SDADC2EN : 1;
-	uint8 SDADC3EN : 1;
-	uint8 reserved7 : 5;
+#define RCC_APB2RSTR (*(uint32*)(RCC+0x0C))
 
-};
+#define SDADC3RST 0x4000000
+#define SDADC2RST 0x2000000
+#define SDADC1RST 0x1000000
+#define TIM19RST 0x80000
+#define TIM17RST 0x40000
+#define TIM16RST 0x20000
+#define TIM15RST 0x10000
+#define USART1RST 0x4000
+#define SPI1RST 0x1000
+#define ADCRST 0x200
+#define SYSCFGRST 0x1
 
-struct RCC_APB1ENR {
-	uint8 TIM2EN : 1;
-	uint8 TIM3EN : 1;
-	uint8 TIM4EN : 1;
-	uint8 TIM5EN : 1;
-	uint8 TIM6EN : 1;
-	uint8 TIM7EN : 1;
-	uint8 TIM12EN : 1;
-	uint8 TIM13EN : 1;
-	uint8 TIM14EN : 1;
-	uint8 TIM18EN : 1;
-	uint8 reserved : 1;
-	uint8 WWDGEN : 1;
-	uint8 reserved2 : 2;
-	uint8 SPI2EN : 1;
-	uint8 SPI3EN : 1;
-	uint8 reserved3 : 1;
-	uint8 USART2EN : 1;
-	uint8 USART3EN : 1;
-	uint8 reserved4 : 2;
-	uint8 I2C1EN : 1;
-	uint8 I2C2EN : 1;
-	uint8 USBEN : 1;
-	uint8 reserved5 : 1;
-	uint8 CANEN : 1;
-	uint8 DAC2EN : 1;
-	uint8 reserved6 : 1;
-	uint8 PWREN : 1;
-	uint8 DAC1EN : 1;
-	uint8 CECEN : 1;
-	uint8 reserved7 : 1;
-};
+#define RCC_APB1RSTR (*(uint32*)(RCC+0x10))
 
-struct RCC_BDCR {
-	uint8 LSEON : 1;
-	uint8 LSERDT : 1;
-	uint8 LSEBYP : 1;
-	uint8 LSEDRV : 2;
-	uint8 reserved : 3;
-	uint8 RTCSEL : 2;
-	uint8 reserved2 : 5;
-	uint8 RTCEN : 1;
-	uint8 RDRST : 1;
-	uint16 reserved3 : 15;
-};
+#define CECRST 0x40000000
+#define DAC1RST 0x20000000
+#define PWRRST 0x10000000
+#define DAC2RST 0x4000000
+#define CANRST 0x2000000
+#define USBRST 0x800000
+#define I2C2RST 0x400000
+#define I2C1RST 0x200000
+#define USART3RST 0x40000
+#define USART2RST 0x20000
+#define SPI3RST 0x8000
+#define SPI2RST 0x4000
+#define WWDGRST 0x800
+#define TIM18RST 0x200
+#define TIM14RST 0x100
+#define TIM13RST 0x80
+#define TIM12RST 0x40
+#define TIM7RST 0x20
+#define TIM6RST 0x10
+#define TIM5RST 0x8
+#define TIM4RST 0x4
+#define TIM3RST 0x2
+#define TIM2RST 0x1
 
-struct RCC_CSR {
-	uint8 LSION : 1;
-	uint8 LSIRDY : 1;
-	uint32 reserved : 21;
-	uint8 V18PWRRSTF : 1;
-	uint8 RMVF : 1;
-	uint8 OBLRSTF : 1;
-	uint8 PINRSTF : 1;
-	uint8 PORRSTF : 1;
-	uint8 SFTRSTF : 1;
-	uint8 IWDGRSTF : 1;
-	uint8 WWDGRSTF : 1;
-	uint8 LPWRRSTF : 1;
-};
+#define RCC_AHBENR (*(uint32*)(RCC+0x14))
 
-struct RCC_AHBRSTR {
-	uint32 reserved : 17;
-	uint8 IOPARST : 1;
-	uint8 IOPBRST : 1;
-	uint8 IOPCRST : 1;
-	uint8 IOPDRST : 1;
-	uint8 IOPERST : 1;
-	uint8 IOPFRST : 1;
-	uint8 reserved2 : 1;
-	uint8 TSCRST : 1;
-	uint8 reserved3 : 7;
-};
+#define TSCEN 0x1000000
+#define IOPFEN 0x400000
+#define IOPEEN 0x200000
+#define IOPDEN 0x100000
+#define IOPCEN 0x80000
+#define IOPBEN 0x40000
+#define IOPAEN 0x20000
+#define CRCEN 0x40
+#define FLITFEN 0x10
+#define SRAMEN 0x4
+#define DMA2EN 0x2
+#define DMA1EN 0x1
 
-struct RCC_CFGR2 {
-	uint8 PREDIV : 4;
-	uint32 reserved : 28;
-};
+#define RCC_APB2ENR (*(uint32*)(RCC+0x18))
 
-struct RCC_CFGR3 {
-	uint8 USART1SW : 2;
-	unsigned reserved : 2;
-	uint8 I2C1SW : 1;
-	uint8 I2C2SW : 1;
-	uint8 CECSW : 1;
-	uint16 reserved2 : 9;
-	uint8 USART2SW : 2;
-	uint8 USART3SW : 2;
-	uint16 reserved3 : 12;
-};
+#define SDADC3EN 0x4000000
+#define SDADC2EN 0x2000000
+#define SDADC1EN 0x1000000
+#define DBGMCUEN 0x400000
+#define TIM19EN 0x80000
+#define TIM17EN 0x40000
+#define TIM16EN 0x20000
+#define TIM15EN 0x10000
+#define USART1EN 0x4000
+#define SPI1EN 0x1000
+#define ADCEN 0x200
+#define SYSCFGEB 0x1
 
-struct RCCx {
-	struct RCC_CR CR;
-	struct RCC_CFGR CFGR;
-	struct RCC_CIR CIR;
-	struct RCC_APB2RSTR APB2RSTR;
-	struct RCC_APB1RSTR APB1RSTR;
-	struct RCC_AHBENR AHBENR;
-	struct RCC_APB2ENR APB2ENR;
-	struct RCC_APB1ENR APB1ENR;
-	struct RCC_BDCR BDCR;
-	struct RCC_CSR CSR;
-	struct RCC_AHBRSTR AHBRSTR;
-	struct RCC_CFGR2 CFGR2;
-	struct RCC_CFGR3 CFGR3;
-};
+#define RCC_APB1ENR (*(uint32*)(RCC+0x1C))
 
-#define RCC ((struct RCCx*)0x40021000)
+#define CECEN 0x40000000
+#define DAC1EN 0x20000000
+#define PWREN 0x10000000
+#define DAC2EN 0x4000000
+#define CANEN 0x2000000
+#define USBEN 0x800000
+#define I2C2EN 0x400000
+#define I2C1EN 0x200000
+#define USART3EN 0x40000
+#define USART2EN 0x20000
+#define SPI3EN 0x8000
+#define SPI2EN 0x4000
+#define WWDGEN 0x800
+#define TIM18EN 0x200
+#define TIM14EN 0x100
+#define TIM13EN 0x80
+#define TIM12EN 0x40
+#define TIM7EN 0x20
+#define TIM6EN 0x10
+#define TIM5EN 0x8
+#define TIM4EN 0x4
+#define TIM3EN 0x2
+#define TIM2EN 0x1
+
+#define RCC_BDCR (*(uint32*)(RCC+0x20))
+
+#define BDRST 0x10000
+#define RTCEN 0x8000
+#define RTCSEL(sel) ((sel 0x3) << 8)
+#define LSEDRV(drv) ((drv & 0x3) << 3)
+#define LSEBYP 0x4
+#define LSERDY 0x2
+#define LSEON 0x1
+
+#define RCC_CSR (*(uint32*)(RCC+0x24))
+
+#define LPWRRSTF 0x80000000
+#define WWDGRSTF 0x40000000
+#define IWDGRSTF 0x20000000
+#define SFTRSTF 0x10000000
+#define PORRSTF 0x8000000
+#define PINRSTF 0x4000000
+#define OBLRSTF 0x2000000
+#define RMVF 0x1000000
+#define V18PWRRSTF 0x800000
+#define LSIRDY 0x2
+#define LSION 0x1
+
+#define RCC_AHBRSTR (*(uint32*)(RCC+0x28))
+
+#define TSCRST 0x1000000
+#define IOPFRST 0x400000
+#define IOPERST 0x200000
+#define IOPDRST 0x100000
+#define IOPCRST 0x80000
+#define IOPBRST 0x40000
+#define IOPARST 0x20000
+
+#define RCC_CFGR2 (*(uint32*)(RCC+0x2C))
+
+#define PREDIV(div) (div & 0xF)
+
+#define RCC_CFGR3 (*(uint32*)(RCC+0x30))
+
+#define USART3SW(sw) ((sw & 0x3) << 18)
+#define USART2SW(sw) ((sw & 0x3) << 16)
+#define CECSW 0x40
+#define I2C2SW 0x20
+#define I2C1SW 0x10
+#define USART1SW(sw) (sw & 0x3)
+
+
 
 #pragma endregion
 
