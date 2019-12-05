66bd315f864b99f199d74d969cb6d72b335d3c0f
JeppeSRC <theholyhorse.dev@gmail.com>
2019-12-05 19:34:46 +0100

    Removed everything in main to start over

--- MicroCode/src/main.c
+++ MicroCode/src/main.c
@@ -7,29 +7,6 @@
 
 #define DEBUG 1
 
-volatile uint16 vSet = 0; //Variable used by the encoder
-volatile uint16 cSet = 0; //Variable used by the encoder
-
-void EXTI0_Handler() { //Encoder 0 (Voltage); Interrupt 6
-	//vSet += (GPIOA_IDR & IDR(1, 1)) >> 1;
-
-	if (vSet > 2000) vSet = 2000;
-
-	vSet = 1;
-
-	EXTI_PR |= 0x01;
-}
-
-void EXTI5_9_Handler() { //Encoder 1 (Current); Interrupt 23
-	//cSet += (GPIOB_IDR & IDR(7, 1)) >> 7;
-
-	if (cSet > 400) cSet = 400;
-
-	vSet = 2;
-
-	EXTI_PR |= 0x40;
-}
-
 #define ATTRIB_OUTPUT 0x01
 #define ATTRIB_CLIMIT 0x02
 
@@ -38,12 +15,10 @@ volatile uint8 attributes;
 void EXTI2TS_Handler() { // Switch (Encoder 0) output on off toggle; Interrupt 8
 	attributes ^= ATTRIB_OUTPUT;
 
-	vSet = 3;
 	EXTI_PR |= 0x4;
 }
 
 void EXTI3_Handler() { //Swicth (Encoder 1); Interrupt 9
-	vSet = 4;
 	EXTI_PR |= 0x8;
 }
 
@@ -52,77 +27,6 @@ char line2Buffer[17];
 
 int main() {
 	Initialize();
-	
-	//memzero(line1Buffer, 17);
-	
-//	sprintf(line1Buffer, 16, "Dank %u");
 
-	/*uint32ToString(1000, 10, line1Buffer, 0, 0);
 
-	DisplayPrint(0, line1Buffer);*/
-	DisplayPrint(0x40, "EXTI");
-	
-	while (1) {
-		if (vSet == 1) {
-			DisplayPrint(0x44, "0  ");
-		} else if (vSet == 2) {
-			DisplayPrint(0x44, "5_9");
-		} else if (vSet == 3) {
-			DisplayPrint(0x44, "2TS");
-		} else if (vSet == 4) {
-			DisplayPrint(0x44, "3  ");
-		}
-	}
-
-	asm("b .");
-	/*
-	DisplayControl(1, 0, 0);
-
-#if DEBUG
-	DisplayPrint(0, "Stating....");
-	DisplayPrint(0x40, "Debug Mode");
-	Delay(2);
-#endif
-
-	DisplayClear();
-
-	while (1) {
-		DelayMillis(20); 
-
-		if (attributes & ATTRIB_OUTPUT) {
-			DAC1_DHR12R2 = vSet * 2;
-			DAC2_DHR12R1 = cSet * 5;
-
-			GPIOB_BSRR |= BS(8); // Setting OUTPUT_ON LED ON
-		} else {
-			DAC1_DHR12R2 = 0;
-			DAC2_DHR12R1 = 0;
-
-			GPIOB_BSRR |= BR(8); // Setting OUTPUT_ON LED OFF
-		}
-
-		if (GPIOA_IDR & IDR(4, 1)) { // Current is not limited
-			GPIOA_BSRR |= BR(4);  //Wrong
-		} else { 
-			GPIOA_BSRR |= BS(4);
-		}
-
-		while (!(SDADC1_ISR & 0x08));
-		
-		uint16 vRead = (SDADC1_RDATAR >> 4) >> 1;
-
-		while (!(SDADC2_ISR & 0x08));
-
-		uint16 cRead = (SDADC2_RDATAR >> 4) / 5;
-
-		memset(line1Buffer, 0, sizeof(line1Buffer));
-		memset(line2Buffer, 0, sizeof(line2Buffer));
-
-		sprintf(line1Buffer, sizeof(line1Buffer), "SET %u.%uV %u.%uA", vSet / 100, vSet % 100, cSet / 100, cSet % 100);
-		sprintf(line2Buffer, sizeof(line2Buffer), "OUT %u.%uV %u.%uA", vRead / 100, vRead % 100, cRead / 100, cRead % 100);
-
-		DisplayPrint(0, line1Buffer);
-		DisplayPrint(0x40, line2Buffer);
-	}
-	*/
 }
--- MicroCode/src/sys/setup.c
+++ MicroCode/src/sys/setup.c
@@ -142,16 +142,5 @@ void InitializeSDADC() {
 }
 
 void InitializeEncoders() {
-	SYSCFG_EXTICR1 = 0; //Set EXTI0 interrupt to trigger on PA0, EXTI2 on PA2, EXTI3 on PA3
-	SYSCFG_EXTICR2 = 0x100; //Set EXTI6 interrupt to trigger on PB6
-	
-	EXTI_IMR |= 0x0D; // Enabled EXTI0, EXTI2, EXTI3 and EXTI6 interrupts
-	EXTI_FTSR |= 0x0D; //Set EXTI0, EXTI2, EXTI3 and EXTI6 interrupts to trigger on rising edge
-
-	EnableInterrupt(6);
-	EnableInterrupt(8);
-	EnableInterrupt(9);
-	EnableInterrupt(23);
-
-	// ISR handlers are implemented in main.c
+a
 }
