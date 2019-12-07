db2c3d913eec6ad7496350de74d9c6b836540243
JeppeSRC <theholyhorse.dev@gmail.com>
2019-12-07 23:00:13 +0100

    added const

--- MicroCode/src/sys/memory.h
+++ MicroCode/src/sys/memory.h
@@ -3,5 +3,5 @@
 #include <core/def.h>
 
 void memset(void* dst, uint8 v, uint32 size);
-void memcpy(void* dst, void* src, uint32 size);
+void memcpy(void* dst, const void* src, uint32 size);
 void memzero(void* dst, uint32 size);
