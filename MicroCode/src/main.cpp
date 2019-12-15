268d3de9bf50a45de52f4669388e3e26585b6f82
JeppeSRC <theholyhorse.dev@gmail.com>
2019-12-15 13:47:36 +0100

    Removed unsused stuff

--- MicroCode/src/main.cpp
+++ MicroCode/src/main.cpp
@@ -5,24 +5,6 @@
 #include <core/string.h>
 #include <sys/sys.h>
 
-#define DEBUG 1
-
-#define ATTRIB_OUTPUT 0x01
-#define ATTRIB_CLIMIT 0x02
-
-volatile uint8 attributes;
-
-void EXTI2TS_Handler() { // Switch (Encoder 0) output on off toggle; Interrupt 8
-	attributes ^= ATTRIB_OUTPUT;
-
-	EXTI_PR |= 0x4;
-}
-
-void EXTI3_Handler() { //Swicth (Encoder 1); Interrupt 9
-	EXTI_PR |= 0x8;
-}
-
-
 char line1Buffer[17] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
 char line2Buffer[17] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
 
