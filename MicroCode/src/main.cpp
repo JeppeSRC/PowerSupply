0761843faccca46140e25c7d3b71cdba818f6811
JeppeSRC <theholyhorse.dev@gmail.com>
2020-03-29 10:21:14 +0200

    Setting voltage to dac and printing to screen

--- MicroCode/src/main.cpp
+++ MicroCode/src/main.cpp
@@ -12,8 +12,24 @@ char line2Buffer[17] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
 int main() {
 	Initialize();
 
+	uint16 vSet = 0;
+	uint16 iSet = 0;
+
+	Display::Print(0x00, "SET 00.00V 0.00A");
+	Display::Print(0x40, "OUT 00.00V 0.00A");
+
 	while (true) {
-		
+		if (vSet != TIM19_CNT) {
+			DAC2_DHR12R1 = vSet = TIM19_CNT;
+
+			Display::Printf(0x04, "%02U.%02UV", vSet / 100, vSet % 100);
+		}
+
+		if (iSet != TIM4_CNT) {
+			DAC1_DHR12R2 = iSet = TIM4_CNT;
+
+			Display::Printf(0x0B, "%01U.%02A", iSet / 100, iSet % 100);
+		}
 	}
 
 	asm("b .");
