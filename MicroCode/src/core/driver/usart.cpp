2b5965c1496ec9752c725245cbf6d626d828e0e5
JeppeSRC <theholyhorse.dev@gmail.com>
2020-05-22 21:08:18 +0200

    Fixed USART

--- MicroCode/src/core/driver/usart.cpp
+++ MicroCode/src/core/driver/usart.cpp
@@ -1,24 +1,21 @@
 #include "usart.h"
 #include <sys/registers/registers.h>
 #include <sys/time.h>
+#include <sys/setup.h>
 
 void USART::Initialize() {
 	RCC_APB2ENR |= USART1EN;
 
-	DelayMicros(10);
-
-	USART1_BRR = 48000000 / 5000; //9600
-	USART1_CR1 |= 0x05;
-	
-	DelayMicros(10);
-
-	while (!(USART1_ISR & 0x70));
+	USART1_BRR = 48000000 / 1000000; //1Mbps
+	USART1_CR1 |= 0x01; 
 }
 
 void USART::Send(uint8 data) {
+	USART1_CR1 |= 0x08;
 	while (!(USART1_ISR & 0x80));
 
 	USART1_TDR = data;
 
-	while (!(USART1_ISR & 0x70));
+	while (!(USART1_ISR & 0x40));
+	USART1_CR1 &= ~0x08;
 }
