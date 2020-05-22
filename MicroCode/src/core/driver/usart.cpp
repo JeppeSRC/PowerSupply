2b74c31d4cb7e943dd9175b794b2dfb5361d1e62
JeppeSRC <theholyhorse.dev@gmail.com>
2020-05-22 21:37:48 +0200

    More USART functions

--- MicroCode/src/core/driver/usart.cpp
+++ MicroCode/src/core/driver/usart.cpp
@@ -2,11 +2,14 @@
 #include <sys/registers/registers.h>
 #include <sys/time.h>
 #include <sys/setup.h>
+#include <core/string.h>
 
-void USART::Initialize() {
+char USART::tmpBuffer[128];
+
+void USART::Initialize(uint32 baud) {
 	RCC_APB2ENR |= USART1EN;
 
-	USART1_BRR = 48000000 / 1000000; //1Mbps
+	USART1_BRR = 48000000 / baud;
 	USART1_CR1 |= 0x01; 
 }
 
@@ -18,4 +21,34 @@ void USART::Send(uint8 data) {
 
 	while (!(USART1_ISR & 0x40));
 	USART1_CR1 &= ~0x08;
+}
+
+void USART::Send(uint8* data, uint16 len) {
+	USART1_CR1 |= 0x08;
+
+	for (uint32 i = 0; i < len; i++) {
+		while (!(USART1_ISR & 0x80));
+
+		USART1_TDR = data[i];
+	}
+
+	while (!(USART1_ISR & 0x40));
+	USART1_CR1 &= ~0x08;
+}
+
+void USART::Print(const char* string) {
+	uint32 len = strlen(string);
+
+	Send((uint8*)string, (uint16)len);
+}
+
+void USART::Printf(const char* fmt, ...) {
+	va_list list;
+	va_start(list, fmt);
+
+	uint16 len = vsprintf(tmpBuffer, sizeof(tmpBuffer), fmt, list);
+
+	va_end(list);
+
+	Send((uint8*)tmpBuffer, len);
 }
--- MicroCode/src/core/driver/usart.h
+++ MicroCode/src/core/driver/usart.h
@@ -4,8 +4,12 @@
 
 class USART {
 private:
+	static char tmpBuffer[128];
 	
 public:
-	static void Initialize();
+	static void Initialize(uint32 baud);
 	static void Send(uint8 data);
+	static void Send(uint8* data, uint16 len);
+	static void Print(const char* string);
+	static void Printf(const char* fmt, ...);
 };
